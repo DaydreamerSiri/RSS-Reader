d6c30d3abaa9e428213431b8d24d7034d53f7f4c
Sehri Singh <3menkill3@gmail.com>
2022-05-29 01:17:26 +0200

    Filter und Link öffnen eingefügt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Xml;
 
 namespace RSS_Reader
 {
@@ -20,9 +23,62 @@ namespace RSS_Reader
     /// </summary>
     public partial class MainWindow : Window
     {
+        Reader reader = new Reader("https://azurecomcdn.azureedge.net/de-de/updates/feed/");
+
         public MainWindow()
         {
             InitializeComponent();
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(FeedDisplay.ItemsSource);
+
         }
+
+        private void GoToUpdate_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.OpenBrowser((sender as Button).Tag as string);   
+        }
+
+        private void GoToUpdate_DoubleClick(object sender, RoutedEventArgs e)
+        {
+            this.OpenBrowser((sender as TextBlock).Tag as string);
+        }
+
+        private void OpenBrowser(string Path)
+        {
+            try
+            {
+                Process.Start(Path);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    Path = Path.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {Path}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", Path);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", Path);
+                }
+                else
+                {
+                    MessageBox.Show("Bitte nutzen sie einen unterstütztes System (Win/Lin/Mac)", "Falsches Betriebssystem", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                }
+            }
+        }
+
+        private void FilterBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            //CollectionViewSource.GetDefaultView(FeedDisplay.ItemsSource).Refresh();
+
+            FeedDisplay.Items.Filter = target => ((XmlElement)target)["title"].InnerText.Contains(FilterBox.Text);
+
+        }
+
     }
 }
